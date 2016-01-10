aecaeb02f0d4359a75292a528fd3274d1a474b4d
Himanshu Chauhan <hchauhan1404@outlook.com>
2016-01-10 14:35:52 +0530

    Schedule Page functionally done, designing needed.

--- js/schedule.js
+++ js/schedule.js
@@ -1,25 +1,44 @@
+$(document).ready(function() {
+	var fromDate, toDate, temp;
+	
+	//For the time when page loads.
+	fromDate = 1;
+	toDate = 19;
+	animateDate(fromDate, toDate);
+	fromDate = toDate;
+	
+	//Scrolling Plugin.
+ 	$('#myContainer').multiscroll({
+ 		sectionsColor: ['#5ad0ff', '#ffffff', '#ffdd00', '#5ad0ff'], // bgcolors of section.
+     	anchors: ['feb19', 'feb20', 'feb21', 'misc'],
+     	menu: '#menu',
+        navigation: true,
+    	navigationTooltips: ['Feb 19', 'Feb 20', 'Feb 21', 'Misc'],
+		
 
-var date = function(){
-        
-};
-
-var resetDate = function() {
-    //Copy this code to your javascript file.
-    var n = $('.outer-date').data("to_date");
-    for(var i = 1; i < n; i++){
-        $('.date').animate({top: "+=100"}, 100); 
-    }
-} 
-
-var iScrollPos = 0;
-$(document).ready(function () {
-   $('.outer-date').each(function(i, element){
-      //Copy this code to your javascript file.
-        var n = $('this').data("to_date");
-        for(var i = 1; i < n; i++){
-            $('.date').animate({top: "-=100"}, 100); 
+     	afterLoad: function(anchor, index){
+			temp = getDate();
+			if(temp !== undefined) {
+				toDate = temp;
+			}
+            animateDate(fromDate, toDate);
+			fromDate = toDate;     	
         }
-   });
+    });
 });
 
+var getDate = function() {
+    return $(".ms-left").children(".active").data("to_date");
+};
 
+var animateDate = function(fromDate, toDate) {
+	if(fromDate < toDate){
+		for(var i=fromDate; i < toDate; i++){
+			$('.date').animate({top: "-=100"}, 100);
+		}
+	} else if(fromDate > toDate) {
+		for(var j=fromDate; j > toDate; j--){
+			$('.date').animate({top: "+=100"}, 100);
+		}
+	}
+};
